53a1b6dd709981b446e468fff76d7ad24445ccac
Shielarose Gandhi <shielarosegandhi@gmail.com>
2021-04-22 21:43:57 -0400

    set up files

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Pixi JS Tutorial</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  data() {
+    return {
+
+    };
+  },
+  mounted() {
+  }
+};
+</script>
